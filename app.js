9bf37909fad42be403e472d4984e5f24f6f51119
YhanMassato <yhanashihara@gmail.com>
2024-04-16 20:24:38 -0300

    Yhan: Exibição dos dados no HTML e Cadastro dos livros

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
+const livroList = document.querySelector('#book-list')
+
+
+
+function renderBook(doc){
+    let list    = document.createElement("li");
+    let titulo  = document.createElement("span")
+    let autor   = document.createElement("span")
+
+    list.setAttribute('data-id', doc.id)
+    titulo.textContent = doc.data().titulo
+    autor.textContent = doc.data().autor   
+    
+    list.appendChild(titulo)
+    list.appendChild(autor)
+
+    livroList.appendChild(list)
+}
+
+
+
 db.collection('libre-firestore')
     .get()
     .then(
         (snapshot) => {
             // console.log(snapshot.docs)
             snapshot.docs.forEach(doc => {
-                console.log(doc.data())      
+                renderBook(doc)
             });
         }
     )
 
+//--------------------------
+
+const form = document.querySelector('#add-book-form')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    db.collection('libre-firestore')
+        .add({
+            autor: form.autor.value,
+            titulo: form.titulo.value
+        })
+        .then(()=>{
+            form.autor.value = ""
+            form.titulo.value = ""
+            window.location.reload()
+        })
+})
